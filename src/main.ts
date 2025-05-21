d93d91ce7ed62cd0a3a060b7a33788150c84a013
penaherrera <carlos.cpa26@gmail.com>
2025-05-21 11:34:02 -0600

    adding base config and http exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, VersioningType } from '@nestjs/common';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrapper');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors({ origin: '*' });
+  app.setGlobalPrefix('api');
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
+  await app.listen(3000);
+
+  logger.log(`Application running on port 3000`);
 }
 bootstrap();
