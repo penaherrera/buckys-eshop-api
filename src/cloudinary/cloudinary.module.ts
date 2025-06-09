f8df960c436acacf3e98a053e20e1cf3d2bb945d
penaherrera <carlos.cpa26@gmail.com>
2025-06-09 12:07:42 -0600

    refactor: adding cloudinary config to cloudinary module

--- src/cloudinary/cloudinary.module.ts
+++ src/cloudinary/cloudinary.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CloudinaryService } from './services/cloudinary.service';
 import { CloudinaryProvider } from './cloudinary.provider';
+import { ConfigModule } from '@nestjs/config';
+import cloudinaryConfig from './config/cloudinary.config';
 
 @Module({
+  imports: [ConfigModule.forFeature(cloudinaryConfig)],
   providers: [CloudinaryService, CloudinaryProvider],
   exports: [CloudinaryProvider, CloudinaryService],
 })
